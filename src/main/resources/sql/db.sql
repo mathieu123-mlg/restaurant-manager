53f178ba1f62181abd345866ba1604c922475aad
mathieu <mathieu.user24@gmail.com>
2025-12-17 20:22:56 +0300

    feat: init db.sql

---
+++ src/main/resources/sql/db.sql
@@ -0,0 +1,15 @@
+create database mini_dish_db;
+
+create user mini_dish_db_manager with password '123456';
+
+grant connect on database mini_dish_db to mini_dish_db_manager;
+
+\c mini_dish_db
+
+grant create on schema public to mini_dish_db_manager;
+
+alter default privileges in schema public
+      grant select, update, insert, delete on tables to mini_dish_db_manager;
+
+alter default privileges in schema public
+      grant usage, select, update on sequences to mini_dish_db_manager;
