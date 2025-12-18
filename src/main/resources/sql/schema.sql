db0bc9412617ca57992989f1e397576bc85b183d
mathieu <mathieu.user24@gmail.com>
2025-12-18 19:06:53 +0300

    feat: init table dish and ingredient in database mini_dish_db

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,18 @@
+\c mini_dish_db
+
+create table dish (
+    id serial primary key,
+    name varchar not null,
+    dish_type varchar not null check
+        (dish_type in ('START', 'MAIN', 'DESSERT')) default 'START'
+);
+
+create table ingredient (
+    id serial primary key,
+    name varchar unique not null,
+    price numeric(10,2) default 0,
+    category varchar NOT NULL
+        CHECK (category IN
+               ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER')),
+    id_dish int references dish(id)
+);
