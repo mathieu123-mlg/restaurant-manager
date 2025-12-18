85645ad8390c128514c279e723bd69cf2df5ded0
mathieu <mathieu.user24@gmail.com>
2025-12-18 19:07:52 +0300

    feat: insert data in dish and ingredient

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,15 @@
+\c mini_dish_db
+
+insert into dish (id, name, dish_type)
+VALUES (1, 'Salade fraîche', 'START'),
+       (2, 'Poulet grillé', 'MAIN'),
+       (3, 'Riz au légume', 'MAIN'),
+       (4, 'Gâteau aux chocolat', 'DESSERT'),
+       (5, 'Salade de fruits', 'DESSERT');
+
+insert into ingredient (id, name, price, category, id_dish)
+VALUES (1, 'Laitue', 800.00, 'VEGETABLE', 1),
+       (2, 'Tomate', 600.00, 'VEGETABLE', 1),
+       (3, 'Poulet', 4500.00, 'ANIMAL', 2),
+       (4, 'Chocolat', 3000.00, 'OTHER', 4),
+       (5, 'Beurre', 2500.00, 'DAIRY', 4);
