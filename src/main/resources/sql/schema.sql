4513e87e69fc37741ab2707be1b67731377829e1
mathieu <mathieu.user24@gmail.com>
2026-01-21 01:00:20 +0300

    feat: create type and update name of old type

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -3,9 +3,8 @@
 create table dish
 (
     id        serial primary key,
-    name      varchar not null,
-    dish_type varchar not null check
-        (dish_type in ('STARTER', 'MAIN', 'DESSERT')) default 'STARTER'
+    name      varchar   not null,
+    dish_type dish_type not null default 'STARTER'
 );
 
 create table ingredient
@@ -13,9 +12,7 @@ create table ingredient
     id       serial primary key,
     name     varchar unique not null,
     price    numeric(10, 2) default 0,
-    category varchar        NOT NULL
-        CHECK (category IN
-               ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER')),
+    category category       NOT NULL,
     id_dish  int references dish (id)
 );
 
@@ -23,8 +20,44 @@ create table ingredient
 alter table dish
     add price numeric(10, 2) default null;
 
-update dish set price = 2000 where name = 'Salade fraîche';
-update dish set price = 6000 where name = 'Poulet grillé';
-update dish set price = null where name = 'Riz au légume';
-update dish set price = null where name = 'Gâteau au chocolat';
-update dish set price = null where name = 'Salade de fruit';
+update dish
+set price = 2000
+where name = 'Salade fraîche';
+
+update dish
+set price = 6000
+where name = 'Poulet grillé';
+
+update dish
+set price = null
+where name = 'Riz au légume';
+
+update dish
+set price = null
+where name = 'Gâteau au chocolat';
+
+update dish
+set price = null
+where name = 'Salade de fruit';
+
+
+
+create type dish_type as enum ('STARTER', 'MAIN', 'DESSERT');
+create type category as enum ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
+
+ALTER TABLE dish
+    ALTER COLUMN dish_type drop default;
+
+ALTER TABLE dish
+    ALTER COLUMN dish_type
+        TYPE dish_type
+        USING dish_type::varchar(20)::dish_type;
+
+ALTER TABLE dish
+    ALTER COLUMN dish_type
+        SET DEFAULT 'STARTER'::dish_type;
+
+ALTER TABLE ingredient
+    ALTER COLUMN category
+        TYPE category
+        USING category::varchar(20)::category;
