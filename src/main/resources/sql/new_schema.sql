b2adb1d4c60b5f664e5c0c0f7818bb5c5864a7a1
mathieu <mathieu.user24@gmail.com>
2026-01-24 17:19:55 +0300

    chore: update new_schema.sql

--- src/main/resources/sql/new_schema.sql
+++ src/main/resources/sql/new_schema.sql
@@ -1,12 +1,32 @@
 \c mini_dish_db
 
+create type dish_type as enum ('STARTER', 'MAIN', 'DESSERT');
+
+create table dish
+(
+    id        serial primary key,
+    name      varchar   not null,
+    dish_type dish_type not null default 'STARTER',
+    selling_price numeric(10, 2)
+);
+
+create type ingredient_category as enum ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
+
+create table ingredient
+(
+    id       serial primary key,
+    name     varchar unique      not null,
+    price    numeric(10, 2) default 0,
+    category ingredient_category NOT NULL
+);
+
 create type unit_type as enum ('PCS', 'KG', 'L');
 
 create table dish_ingredient
 (
     id                serial primary key,
-    id_dish           int,
-    id_ingredient     int,
+    id_dish           int references dish(id),
+    id_ingredient     int references ingredient(id),
     quantity_required numeric(10, 2),
     unit              unit_type
 );
