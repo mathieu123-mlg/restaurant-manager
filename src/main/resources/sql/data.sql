723f6fed20f0bbd6f0a642a87ad46c7878c91b80
mathieu <mathieu.user24@gmail.com>
2026-01-25 23:43:37 +0300

    fix: use on conflict to update if data is note inserted

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -7,21 +7,32 @@ VALUES (1, 'Salade fraîche', 'STARTER', 3500.00),
        (4, 'Gâteau aux chocolat', 'DESSERT', 8000.00),
        (5, 'Salade de fruits', 'DESSERT', null)
 ON CONFLICT (id) DO UPDATE
-    set selling_price = EXCLUDED.selling_price;
+    set name          = EXCLUDED.name,
+        dish_type     = EXCLUDED.dish_type,
+        selling_price = EXCLUDED.selling_price;
 
-insert into ingredient (id, name, price, category, id_dish)
+insert into ingredient (id, name, price, category)
 VALUES (1, 'Laitue', 800.00, 'VEGETABLE'),
        (2, 'Tomate', 600.00, 'VEGETABLE'),
        (3, 'Poulet', 4500.00, 'ANIMAL'),
        (4, 'Chocolat', 3000.00, 'OTHER'),
-       (5, 'Beurre', 2500.00, 'DAIRY');
+       (5, 'Beurre', 2500.00, 'DAIRY')
+ON conflict (id) DO UPDATE
+    set name     = EXCLUDED.name,
+        price    = excluded.price,
+        category = excluded.category;
 
 insert into dish_ingredient (id, id_dish, id_ingredient, quantity_required, unit)
 VALUES (1, 1, 1, 0.20, 'KG'),
        (2, 1, 2, 0.15, 'KG'),
        (3, 2, 3, 1.00, 'KG'),
        (4, 4, 4, 0.30, 'KG'),
-       (5, 4, 5, 0.20, 'KG');
+       (5, 4, 5, 0.20, 'KG')
+on conflict (id) do update
+    set id_dish           = excluded.id_dish,
+        id_ingredient     = excluded.id_ingredient,
+        quantity_required = excluded.quantity_required,
+        unit              = excluded.unit;
 
 INSERT INTO stockmovement (id, id_ingredient, quantity, type, unit, creation_datetime)
 VALUES (1, 1, 5.00, 'IN', 'KG', '2024-01-05 08:00:00'),
@@ -33,4 +44,10 @@ VALUES (1, 1, 5.00, 'IN', 'KG', '2024-01-05 08:00:00'),
        (7, 4, 3.00, 'IN', 'KG', '2024-01-05 10:00:00'),
        (8, 4, 0.30, 'OUT', 'KG', '2024-01-06 14:00:00'),
        (9, 5, 2.50, 'IN', 'KG', '2024-01-05 10:00:00'),
-       (10, 5, 0.20, 'OUT', 'KG', '2024-01-06 14:00:00');
+       (10, 5, 0.20, 'OUT', 'KG', '2024-01-06 14:00:00')
+on conflict (id) do update
+    set id_ingredient     = excluded.id_ingredient,
+        quantity          = excluded.quantity,
+        type              = excluded.type,
+        unit              = excluded.unit,
+        creation_datetime = excluded.creation_datetime;
