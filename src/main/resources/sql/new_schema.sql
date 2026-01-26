026a47ae14f9e03e0c99aee356f6b0b51498cd61
mathieu <mathieu.user24@gmail.com>
2026-01-26 11:45:40 +0300

    refactor: refactor script and remove alter table becaus e table is modified

--- src/main/resources/sql/new_schema.sql
+++ src/main/resources/sql/new_schema.sql
@@ -4,9 +4,9 @@ create type dish_type as enum ('STARTER', 'MAIN', 'DESSERT');
 
 create table dish
 (
-    id        serial primary key,
-    name      varchar   not null,
-    dish_type dish_type not null default 'STARTER',
+    id            serial primary key,
+    name          varchar   not null,
+    dish_type     dish_type not null default 'STARTER',
     selling_price numeric(10, 2)
 );
 
@@ -25,8 +25,8 @@ create type unit_type as enum ('PCS', 'KG', 'L');
 create table dish_ingredient
 (
     id                serial primary key,
-    id_dish           int references dish(id),
-    id_ingredient     int references ingredient(id),
+    id_dish           int references dish (id),
+    id_ingredient     int references ingredient (id),
     quantity_required numeric(10, 2),
     unit              unit_type,
     CONSTRAINT unique_dish_ingredient UNIQUE (id_dish, id_ingredient)
@@ -37,11 +37,9 @@ create type movement_type as enum ('IN', 'OUT');
 create table stockmovement
 (
     id                serial primary key,
-    id_ingredient int references ingredient(id),
+    id_ingredient     int references ingredient (id),
     quantity          numeric(10, 2),
     type              movement_type default 'IN' not null,
     unit              unit_type,
     creation_datetime timestamp     default current_timestamp
 );
-
-ALTER TABLE dish_ingredient ADD CONSTRAINT unique_dish_ingredient UNIQUE (id_dish, id_ingredient);
