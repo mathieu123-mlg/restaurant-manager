b5fc5e88b2c8b224e2a627896f5b23d6a9484427
mathieu <mathieu.user24@gmail.com>
2026-01-27 20:55:40 +0300

    fix: update initial stock_movement insert

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -34,13 +34,13 @@ on conflict (id) do update
         quantity_required = excluded.quantity_required,
         unit              = excluded.unit;
 
-INSERT INTO stockmovement (id, id_ingredient, quantity, type, unit, creation_datetime)
+INSERT INTO stock_movement (id, id_ingredient, quantity, type, unit, creation_datetime)
 VALUES (1, 1, 5.00, 'IN', 'KG', '2024-01-05 08:00:00'),
        (2, 1, 0.20, 'OUT', 'KG', '2024-01-06 12:00:00'),
-       (3, 2, 4.00, 'IN', 'KG', '2024-01-06 12:00:00'),
+       (3, 2, 4.00, 'IN', 'KG', '2024-01-05 08:00:00'),
        (4, 2, 0.15, 'OUT', 'KG', '2024-01-06 12:00:00'),
        (5, 3, 10.00, 'IN', 'KG', '2024-01-04 09:00:00'),
-       (6, 3, 1.00, 'OUT', 'KG', '2024-01-06 13:30:00'),
+       (6, 3, 1.00, 'OUT', 'KG', '2024-01-06 13:00:00'),
        (7, 4, 3.00, 'IN', 'KG', '2024-01-05 10:00:00'),
        (8, 4, 0.30, 'OUT', 'KG', '2024-01-06 14:00:00'),
        (9, 5, 2.50, 'IN', 'KG', '2024-01-05 10:00:00'),
