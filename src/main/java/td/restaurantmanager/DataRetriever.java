1db778a0099126704ba4754d8bab5c6cdb2cfcd0
mathieu <mathieu.user24@gmail.com>
2026-02-10 22:50:40 +0300

    feat: Implement findDishById

--- src/main/java/td/restaurantmanager/DataRetriever.java
+++ src/main/java/td/restaurantmanager/DataRetriever.java
@@ -1,10 +1,112 @@
 package td.restaurantmanager;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class DataRetriever {
+    private final DBConnection dbConnection = new DBConnection();
+
     public Dish findDishById(Integer idDish) {
-        throw new RuntimeException("Not Implemented");
+        String sql = """
+                select d.id as dish_id, d.name as dish_name, d.dish_type as dish_type, d.price as dish_price,
+                d_i.id as di_id, d_i.id_ingredient, d_i.quantity_required, d_i.unit,
+                i.id as ingredient_id, i.name as ingredient_name, i.price as ingredient_price, i.category
+                from dish d
+                left join dish_ingredient d_i on d_i.id_dish = d.id
+                left join ingredient i on d_i.id_ingredient = i.id
+                where d.id = ?""";
+        try (Connection conn = dbConnection.getDBConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, idDish);
+            ResultSet rs = ps.executeQuery();
+            Dish dish = null;
+            List<DishIngredient> dishIngredients = new ArrayList<>();
+            while (rs.next()) {
+                if (dish == null) {
+                    dish = new Dish(
+                            rs.getInt("dish_id"),
+                            rs.getString("dish_name"),
+                            DishTypeEnum.valueOf(rs.getString("dish_type")),
+                            rs.getDouble("dish_price"),
+                            new ArrayList<>()
+                    );
+                }
+                if (rs.getInt("di_id") > 0) {
+                    Integer ing_id = rs.getInt("ingredient_id");
+                    Ingredient ingredient = new Ingredient(
+                            ing_id,
+                            rs.getString("ingredient_name"),
+                            rs.getDouble("ingredient_price"),
+                            CategoryEnum.valueOf(rs.getString("category"))
+                    );
+                    DishIngredient dishIngredient = new DishIngredient(
+                            ingredient,
+                            rs.getDouble("quantity_required"),
+                            UnitType.valueOf(rs.getString("unit"))
+                    );
+                    dishIngredients.add(dishIngredient);
+                }
+            }
+            if (dish == null) {
+                throw new RuntimeException("Dish(id=" + idDish + ") not found");
+            }
+            Set<Integer> ingredientIds = dishIngredients.stream()
+                    .map(d_i -> d_i.getIngredient().getId())
+                    .collect(Collectors.toSet());
+            var stockMovementList = fetchStockMovementUsingExistantIds(ingredientIds);
+            dish.setDishIngredients(dishIngredients, stockMovementList);
+            return dish;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Map<Integer, List<StockMovement>> fetchStockMovementUsingExistantIds(Set<Integer> ingredientIds) {
+        if (ingredientIds.isEmpty()) {
+            return new HashMap<>();
+        }
+        String sql = """
+                select id, id_ingredient, quantity, type, unit, creation_datetime
+                from stock_movement where id_ingredient in (%s)""";
+        String inClause = ingredientIds.stream()
+                .map(_ -> "?")
+                .collect(Collectors.joining(","));
+        sql = String.format(sql, inClause);
+
+        try (Connection conn = dbConnection.getDBConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            int index = 1;
+            for (Integer id : ingredientIds) {
+                ps.setInt(index++, id);
+            }
+            ResultSet rs = ps.executeQuery();
+            Map<Integer, List<StockMovement>> stockMovements = new HashMap<>();
+            for (Integer id : ingredientIds) {
+                stockMovements.put(id, new ArrayList<>());
+            }
+            while (rs.next()) {
+                StockMovement stock = new StockMovement(
+                        rs.getInt("id"),
+                        new StockValue(
+                                rs.getDouble("quantity"),
+                                UnitType.valueOf(rs.getString("unit"))
+                        ),
+                        MovementTypeEnum.valueOf(rs.getString("type")),
+                        rs.getTimestamp("creation_datetime").toInstant()
+                );
+                Integer id_ingredient = rs.getInt("id_ingredient");
+                List<StockMovement> stocks = stockMovements.get(id_ingredient);
+                stocks.add(stock);
+                stockMovements.put(id_ingredient, stocks);
+            }
+            return stockMovements;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Ingredient> findIngredients(int page, int size) {
